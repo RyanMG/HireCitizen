df0b5fa6c2606c284406cec1e1d6440d7effead4
David Hall <david.ryan.hall@gmail.com>
2024-12-12 13:50:18 -0800

    feat: snackbar element + notification when user data does not properly load

--- src/app/notifications.tsx
+++ src/app/notifications.tsx
@@ -1,3 +1,4 @@
+import CloseElementIcon from "@/components/closeElementIcon";
 import { useState } from "react";
 
 export default function NotificationSnipe() {
@@ -21,11 +22,7 @@ export default function NotificationSnipe() {
 
           <div className="flex flex-row justify-between border-b-2 border-gray-300 pb-2">
             <div className="text-white text-lg font-bold">Notifications</div>
-            <div className="flex flex-row justify-end" onClick={onNotificationClick}>
-              <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
-                <path d="m336-280 144-144 144 144 56-56-144-144 144-144-56-56-144 144-144-144-56 56 144 144-144 144 56 56ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/>
-              </svg>
-            </div>
+            <CloseElementIcon onClickFn={onNotificationClick} iconFillColor="#e8eaed" />
           </div>
 
         </div>
--- src/app/root.tsx
+++ src/app/root.tsx
@@ -5,24 +5,39 @@ import Loading from "@/components/loading";
 import { useQuery } from "@tanstack/react-query"
 import { fetchActiveUser } from "@/api/userApi";
 import Notification from "./notifications";
+import { useEffect, useState } from "react";
+import Snackbar from "@/components/snackbar";
 
 export default function ProviderRoot({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [hadLoadError, setHadLoadError] = useState<boolean>(false);
 
   const { isLoading, error, data: activeUser } = useQuery({
     queryKey: [`fetch-active-user`],
     queryFn: () => fetchActiveUser(1)
   });
 
+  useEffect(() => {
+    if (error) {
+      console.error(error);
+      setHadLoadError(true);
+    }
+
+    if (activeUser &&  Object.keys(activeUser).length === 0) {
+      console.log("No active user found");
+      setHadLoadError(true);
+    }
+  }, [error, activeUser]);
+
   return (
     <div className="flex flex-row h-full w-full">
 
-      {isLoading && <div>Loading...</div>}
+      {isLoading && <Loading />}
 
-      {error && <Loading />}
+      {hadLoadError && <Snackbar message="Error loading user data" type="error" />}
 
       {activeUser && Object.keys(activeUser).length > 0 &&
         <>
