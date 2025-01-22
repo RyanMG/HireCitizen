a46fe4567dc8af54a3481680a16e40ef7e4e1401
David Ryan Hall <david.ryan.hall@gmail.com>
2025-01-22 15:41:48 -0800

    feat: work history page directs you to the correct job pages
      - the generic job listing for pending or rejected applications
      - a new page for accepted applications where more specific job details can live

--- app/(dashboard)/work-history/accepted-jobs/[id]/page.tsx
+++ app/(dashboard)/work-history/accepted-jobs/[id]/page.tsx
@@ -1,11 +1,18 @@
+import { getJobById } from "@/app/lib/query/job/data";
 import PageWrapper from "@/app/ui/components/pageWrapper";
+import NotificationSnackbar from "@components/notificationSnackbar";
 
 export default async  function JobHistory(props: { params: Promise<{ id: string }> }) {
   const jobParams = await props.params;
   const jobId = jobParams?.id || '';
+  const job = await getJobById(jobId);
+
+  if ('error' in job) {
+    return <NotificationSnackbar type="error" messages={[job.error]} />;
+  }
 
   return (
-    <PageWrapper pageHeaderTitle="UPCOMING JOB TITLE HERE">
+    <PageWrapper pageHeaderTitle={job?.title || ''}>
       <h1>Job ID: {jobId}</h1>
     </PageWrapper>
   );
